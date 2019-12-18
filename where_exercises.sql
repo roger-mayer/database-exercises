f19b7ef598f1d6013c98592b75e829ea3a5c3309
Roger Mayer <rmayer1984@gmail.com>
2019-12-18 16:56:14 -0600

    completed where ex

--- where_exercises.sql
+++ where_exercises.sql
@@ -22,9 +22,10 @@ FROM employees
 WHERE last_name LIKE 'E%' AND last_name LIKE '%E';
 
 -- Find all employees hired in the 90s and born on christmas— 135,214 rows.
--- SELECT emp_no, first_name, last_name, hire_date
--- FROM employees
--- WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31';
+SELECT emp_no, first_name, last_name, hire_date
+FROM employees
+WHERE (hire_date BETWEEN '1990-01-01' AND '1999-12-31')
+  AND birth_date LIKE ('%12-25');
 
 -- Find all employees born on Christmas — 842 rows.
 -- SELECT emp_no, first_name, last_name, birth_date
@@ -36,3 +37,9 @@ WHERE last_name LIKE 'E%' AND last_name LIKE '%E';
 -- FROM employees
 -- WHERE last_name LIKE ('%q%')
 --     OR first_name LIKE ('%q')
+
+# FInd all employees with a 'q' in their last name but not 'qu'
+SELECT emp_no, first_name, last_name
+FROM employees
+WHERE last_name LIKE ('%q%')
+    AND last_name NOT LIKE ('%qu%')
